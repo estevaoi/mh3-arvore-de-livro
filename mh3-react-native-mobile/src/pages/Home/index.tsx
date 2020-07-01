ada9e55004e24b692186d36007035819e48d01a6
Leonardo Marques De Castro Leite <leonardo.leite@prservicos.com.br>
2020-07-01 15:05:24 -0300

    first commit

---
+++ mh3-react-native-mobile/src/pages/Home/index.tsx
@@ -0,0 +1,114 @@
+
+import { useNavigation } from '@react-navigation/native'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+
+const Home = () => {
+
+  const navigation = useNavigation()
+
+  const [param, setParam] = useState<Array<any>>([])
+
+  useEffect(() => {
+    // axios.get<any>('').then(() => {})
+  }, [])
+
+  function handleNavigation() {
+    navigation.navigate('Route', {})
+  }
+  
+  return (
+    <View style={styles.main}>
+      <Text style={styles.title}>Hello World</Text>
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 32,
+    backgroundColor: '#f0f0f5'
+  },
+
+  main: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+
+  title: {
+    color: '#322153',
+    fontSize: 32,
+    fontFamily: 'Ubuntu_700Bold',
+    maxWidth: 260,
+    marginTop: 64,
+  },
+
+  description: {
+    color: '#6C6C80',
+    fontSize: 16,
+    marginTop: 16,
+    fontFamily: 'Roboto_400Regular',
+    maxWidth: 260,
+    lineHeight: 24,
+  },
+
+  footer: {},
+
+  inputIOS: {
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    marginBottom: 8,
+    paddingHorizontal: 24,
+    fontSize: 16,
+  },
+
+  inputAndroid: {
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    marginBottom: 8,
+    paddingHorizontal: 24,
+    fontSize: 16,
+  },
+
+  input: {
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    marginBottom: 8,
+    paddingHorizontal: 24,
+    fontSize: 16,
+  },
+
+  button: {
+    backgroundColor: '#34CB79',
+    height: 60,
+    flexDirection: 'row',
+    borderRadius: 10,
+    overflow: 'hidden',
+    alignItems: 'center',
+    marginTop: 8,
+  },
+
+  buttonIcon: {
+    height: 60,
+    width: 60,
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  buttonText: {
+    flex: 1,
+    justifyContent: 'center',
+    textAlign: 'center',
+    color: '#FFF',
+    fontFamily: 'Roboto_500Medium',
+    fontSize: 16,
+  }
+});
---
+++ mh3-react-spa/src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './styles.css'
+
+const Home: React.FC = () => {
+  return(
+    <div>Hello World</div>
+  )
+}
+
+export default Home
