407a4250f2acd4ae20bd93f61cb19f7f647432c6
Leonardo Marques De Castro Leite <leonardo.leite@prservicos.com.br>
2020-07-03 12:25:37 -0300

    instalacao do material ui

--- mh3-react-spa/src/App.tsx
+++ mh3-react-spa/src/App.tsx
@@ -1,11 +1,58 @@
+import { Container, CssBaseline, useMediaQuery } from '@material-ui/core';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import React from 'react';
 import './App.css';
 import Routes from './routes';
 
 function App() {
 
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: light)');
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? 'dark' : 'light',
+          primary: {
+            contrastText: '#fff',
+            // light: será calculada com base em palette.primary.main,
+            main: '#45d0c1',
+            // dark: será calculada com base em palette.primary.main,
+            // contrastText: será calculada para contrastar com palette.primary.main
+          },
+          secondary: {
+            light: '#8553f4',
+            main: '#8553f4',
+          },
+          text: {
+            primary: '#53656F',
+            secondary: '#53656F',
+            disabled: '#53656F',
+            hint: '#53656F'
+      
+          },
+          common: {
+            black: '#53656F'
+          },
+          // Usado por `getContrastText()` para maximizar o contraste entre
+          // o plano de fundo e o texto.
+          contrastThreshold: 3,
+          // Usado pelas funções abaixo para mudança de uma cor de luminância por aproximadamente
+          // dois índices dentro de sua paleta tonal.
+          // Por exemplo, mude de Red 500 para Red 300 ou Red 700.
+          tonalOffset: 0.2,
+        },
+      }),
+    [prefersDarkMode],
+  )
+
   return (
-    <Routes/>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="sm">
+        <CssBaseline/>
+        {/* <Button onClick={handleChangeTheme}>Trocar tema</Button> */}
+        <Routes/>
+      </Container>
+    </ThemeProvider>
   );
 }
 
--- mh3-react-spa/src/pages/Home/index.tsx
+++ mh3-react-spa/src/pages/Home/index.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './styles.css'
+import { Button } from '@material-ui/core'
 
 const Home: React.FC = () => {
   return(
-    <div>Hello World</div>
+    <div>
+      <Button variant="contained">Default</Button>
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+      <Button variant="contained" color="secondary">
+        Secondary
+      </Button>
+      <Button variant="contained" disabled>
+        Disabled
+      </Button>
+      <Button variant="contained" color="primary" href="#contained-buttons">
+        Link
+      </Button>
+    </div>
   )
 }
 
